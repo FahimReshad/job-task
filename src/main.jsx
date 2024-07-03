dd178a6281143affc08bb24bff438367b8349f12
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-07-04 04:54:34 +0600

    task updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Layout from './MainLayout/Layout.jsx';
-import Home from './Pages/Home/Home.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./MainLayout/Layout.jsx";
+import Home from "./Pages/Home/Home.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
     children: [
       {
-        path: '/',
-        element: <Home></Home>
-      }
-    ]
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
